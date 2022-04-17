2f1cef829143ca034f09c3df2c21b0e4b54728b2
VNT1998 <vinitcalpha@gmail.com>
2022-04-17 19:20:31 +0530

    Added condition on the root to submit a form and redirect to a another block or page

--- app.js
+++ app.js
@@ -4,9 +4,17 @@ const http = require("http");
 // function rqListner(req, res)
 // http.createServer(rqListner);
 const server = http.createServer((req, res) => {
-  console.log(req.url, req.headers, req.method);
+  //   console.log(req.url, req.headers, req.method);
+  const reqUrl = req.url;
+  if (reqUrl === "/") {
+    res.write("<h1>Hello World</h1>");
+    res.write(
+      "<form action='/message' method='POST'><input type='text' name='message'><button type='submit'>Send</button></form>"
+    );
+    return res.end(); //To exit the process and not continue with the further execution
+  }
   res.setHeader("Content-Type", "text/html");
-  res.write("<h1>Hello World</h1>");
+  res.write("<h1>Further block has been executed</h1>");
   res.end();
   //   process.exit();
 });
