0cbe922b9b06d96383f86027af7225f1e7605e0d
VNT1998 <vinitcalpha@gmail.com>
2022-04-17 19:35:31 +0530

    Saving dummy data when clicked on button for redirection

--- app.js
+++ app.js
@@ -1,17 +1,25 @@
 // will add the core module ,you can also add your own js file by using \http or .\http, no need to add .js extension
 const http = require("http");
+const fs = require("fs");
 
 // function rqListner(req, res)
 // http.createServer(rqListner);
 const server = http.createServer((req, res) => {
   //   console.log(req.url, req.headers, req.method);
   const reqUrl = req.url;
+  const reqMethod = req.method;
   if (reqUrl === "/") {
     res.write("<h1>Hello World</h1>");
     res.write(
       "<form action='/message' method='POST'><input type='text' name='message'><button type='submit'>Send</button></form>"
     );
     return res.end(); //To exit the process and not continue with the further execution
+  }
+  if (reqUrl === "/message" && reqMethod === "POST") {
+    fs.writeFileSync("message.txt", "Some dummy text");
+    res.statusCode = 302;
+    res.setHeader("Location", "/");
+    return res.end();
   }
   res.setHeader("Content-Type", "text/html");
   res.write("<h1>Further block has been executed</h1>");
