bc8e61f66d63b219e7312c042dbe8b48338a6de7
VNT1998 <vinitcalpha@gmail.com>
2022-05-01 21:46:41 +0530

    view seperated from the routes and hooked it up to the routes

--- routes/admin.js
+++ routes/admin.js
@@ -1,11 +1,10 @@
+const path = require("path");
 const express = require("express");
 const router = express.Router();
 
 router.get("/add-products", (req, res, next) => {
   console.log("This is another a middleware");
-  res.send(
-    '<form action="/admin/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>'
-  );
+  res.sendFile(path.join(__dirname, "../", "views", "add-product.html"));
 });
 
 router.post("/product", (req, res, next) => {
