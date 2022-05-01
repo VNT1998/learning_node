d148007997c750abe6f65bb8a48d22fab6cb856b
VNT1998 <vinitcalpha@gmail.com>
2022-05-01 21:50:27 +0530

    404 HTML added

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 // will add the core module ,you can also add your own js file by using \http or .\http, no need to add .js extension
+const path = require("path");
 const http = require("http");
 const express = require("express");
 const app = express();
@@ -13,11 +14,11 @@ app.use("/", (req, res, next) => {
   next();
 });
 
-app.use('/admin',adminRoutes);
+app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).send("<h1>Page not found</h1>");
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 });
 // function rqListner(req, res)
 // http.createServer(rqListner);
