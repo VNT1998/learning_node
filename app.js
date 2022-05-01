124c7f0bec129c449e52f084a234e0c4198932f8
VNT1998 <vinitcalpha@gmail.com>
2022-05-01 11:08:43 +0530

    Using get post method instead of use to filter out the request type

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use("/add-products", (req, res, next) => {
     '<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>'
   );
 });
-app.use("/product", (req, res, next) => {
+app.post("/product", (req, res, next) => {
   console.log(req.body);
   res.redirect("/");
 });
