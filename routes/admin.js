df664de2cbec6a6509a7ecf3b97e34fcfdebbf4a
VNT1998 <vinitcalpha@gmail.com>
2022-05-01 22:14:40 +0530

    Added helper utiliy to the app for retriving system dirpath and fixed the navigation bug

--- routes/admin.js
+++ routes/admin.js
@@ -1,10 +1,11 @@
 const path = require("path");
 const express = require("express");
 const router = express.Router();
+const rootDir = require("../utils/path");
 
 router.get("/add-products", (req, res, next) => {
   console.log("This is another a middleware");
-  res.sendFile(path.join(__dirname, "../", "views", "add-product.html"));
+  res.sendFile(path.join(rootDir, "views", "add-product.html"));
 });
 
 router.post("/product", (req, res, next) => {
