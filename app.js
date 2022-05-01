7a0b6ae58a78b8ee84ad792224378c8561bc408e
VNT1998 <vinitcalpha@gmail.com>
2022-05-01 10:56:04 +0530

    Parsing incoming request and and redirecting to new page

--- app.js
+++ app.js
@@ -2,15 +2,29 @@
 const http = require("http");
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 
-app.use("/message", (req, res, next) => {
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use("/", (req, res, next) => {
+  console.log("This is always runs");
+  next();
+});
+
+app.use("/add-products", (req, res, next) => {
   console.log("This is another a middleware");
-  res.send("<h1>Hello World of messages</h1>");
+  res.send(
+    '<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>'
+  );
+});
+app.use("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
 });
 app.use("/", (req, res, next) => {
-  console.log("This is another a middleware");
-  res.send("<h1>Hello World</h1>");
+  res.send("<h1>Hello there from express</h1>");
 });
+
 // function rqListner(req, res)
 // http.createServer(rqListner);
 // const server = http.createServer(app);
