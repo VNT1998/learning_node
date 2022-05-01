d82a79be8fdc7339ba1cd5913726f4a59ceebc05
VNT1998 <vinitcalpha@gmail.com>
2022-05-01 11:48:46 +0530

    Adding 404 page

--- app.js
+++ app.js
@@ -16,6 +16,9 @@ app.use("/", (req, res, next) => {
 app.use(adminRoutes);
 app.use(shopRoutes);
 
+app.use((req, res, next) => {
+  res.status(404).send("<h1>Page not found</h1>");
+});
 // function rqListner(req, res)
 // http.createServer(rqListner);
 // const server = http.createServer(app);
