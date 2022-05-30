ff957b21ce8443fa396146814aea0216960b27a7
VNT1998 <vinitcalpha@gmail.com>
2022-05-30 20:53:39 +0530

    code refractored

--- models/product.js
+++ models/product.js
@@ -1,39 +1,34 @@
 const fs = require("fs");
 const path = require("path");
+const p = path.join(
+    path.dirname(process.mainModule.filename),
+    "data",
+    "produts.json"
+);
+
+getProductsFromFile = cb => {
+    fs.readFile(p, (err, fileContent) => {
+        if (!err) {
+            cb(JSON.parse(fileContent));
+        } else {
+            cb([]);
+        }
+    });
+}
+
 module.exports = class Product {
     constructor(title) {
         this.title = title;
     }
     save() {
-        const p = path.join(
-            path.dirname(process.mainModule.filename),
-            "data",
-            "produts.json"
-        );
-        fs.readFile(p, (err, fileContent) => {
-            let products = [];
-            if (!err) {
-                products = JSON.parse(fileContent);
-            }
+        getProductsFromFile(products => {
             products.push(this);
             fs.writeFile(p, JSON.stringify(products), (err) => {
                 console.log(err);
-            });
+            })
         });
     }
     static fetchAll(cb) {
-        const p = path.join(
-            path.dirname(process.mainModule.filename),
-            "data",
-            "produts.json"
-        );
-        fs.readFile(p, (err, fileContent) => {
-            if (!err) {
-                cb(JSON.parse(fileContent));
-            } else {
-                cb([]);
-            }
-
-        });
+        getProductsFromFile(cb);
     }
 };
